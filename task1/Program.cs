634eacbf34edd6466766aeeb874f820ef33ac163
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-13 01:19:45 +0300

    bla

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,6 +10,26 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            double epsilon = 1.0;
+
+            while (1.0 + epsilon != 1.0)
+            {
+                epsilon /= 2.0;
+            }
+
+            epsilon *= 2.0;
+
+            Console.WriteLine("Machine epsilon (double precision): " + epsilon);
+
+            float epsilonSingle = 1.0f;
+            while (1.0f + epsilonSingle != 1.0f)
+            {
+                epsilonSingle /= 2.0f;
+            }
+            epsilonSingle *= 2.0f;
+
+            Console.WriteLine("Machine epsilon (single precision): " + epsilonSingle);
+            Console.ReadLine();
         }
     }
 }
