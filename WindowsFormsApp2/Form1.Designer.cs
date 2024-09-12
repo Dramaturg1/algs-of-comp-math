c3d7408b9fabafa606e74ad16c94fe46859d0f9e
Dramaturg1 <widowmakerNoOver@outlook.com>
2024-09-13 01:51:05 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(0, 0);
+            this.zedGraphControl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(913, 445);
+            this.zedGraphControl2.TabIndex = 0;
+            this.zedGraphControl2.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(913, 445);
+            this.Controls.Add(this.zedGraphControl2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double f(double x)
+        {
+            double[] array = { 1, -18, 144, -672, 2016, -4032, 5376, -4608, 2304, -512 };
+            double p = array[0] * x + array[1];
+            for (int i = 2; i < array.Length; i++)
+            {
+                p = p * x + array[i];
+            }
+            return p;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphPane pane = zedGraphControl2.GraphPane;
+            pane.CurveList.Clear();
+            PointPairList list = new PointPairList();
+
+            double xmin = 1.92;
+            double xmax = 2.08;
+
+            for (double x = xmin; x <= xmax; x += 0.0001)
+            {
+                list.Add(x, f(x));
+            }
+            LineItem myCurve = pane.AddCurve("(x-2)^9", list, Color.Magenta, SymbolType.None);
+            zedGraphControl2.AxisChange();
+            zedGraphControl2.Invalidate();
+        }
+
+    }
+}
